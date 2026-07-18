8f6463b2fb4c36ebfab6a0fcb69356c9a68dd938
Adarsh-Dev-Git <adarshpanakkattu@gmail.com>
2026-07-18 23:59:46 +0530

    fix: restore missing div in DigitalTwin after memoization

--- frontend/src/components/DigitalTwin.tsx
+++ frontend/src/components/DigitalTwin.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const DigitalTwin = React.memo(function DigitalTwin() {
   return (
+    <div className="panel col-span-1 lg:col-span-2 row-span-2 relative min-h-[400px]">
       <div className="panel-header flex justify-between items-center">
         <span>Stadium Digital Twin</span>
         <div className="flex gap-2 text-xs">
